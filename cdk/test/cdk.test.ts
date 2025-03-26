78ea68729d03d2a7650adf1f964352e591211a1d
lgklsv <kolosov20011@gmail.com>
2025-03-26 22:02:13 +0400

    🎉 Init cdk stack

---
+++ cdk/test/cdk.test.ts
@@ -0,0 +1,7 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cdk from '../lib/cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-stack.ts
+// test('SQS Queue Created', () => {});
