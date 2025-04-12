12268949aa4a6962e2d4d54d6669c0de40c37680
lgklsv <kolosov20011@gmail.com>
2025-04-12 18:38:32 +0400

    Add api gateway proxy to enable https for aws ebs deployment

--- cdk/bin/nest-lambda.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { NestLambdaStack } from '../lib/nest-lambda-stack';
-
-const app = new cdk.App();
-new NestLambdaStack(app, 'NestLambdaStack', {});
